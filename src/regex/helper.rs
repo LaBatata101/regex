53c11c1cbda1ece9b8517b3a665249ca9c46659d
LaBatata101 <labatata101@linuxmail.org>
2022-10-29 18:10:48 -0300

    remove useless function

--- src/regex/helper.rs
+++ src/regex/helper.rs
@@ -8,26 +8,3 @@ pub fn calculate_hash<T: Hash>(object: &T) -> u64 {
     object.hash(&mut hasher);
     hasher.finish()
 }
-
-pub fn reduce_string_to_range(strings: &[String]) -> String {
-    for str in strings {
-        dbg!(str
-            .chars()
-            .skip(1)
-            .zip(str.chars().skip(2))
-            .collect::<Vec<(char, char)>>());
-        // for (c1, c2) in str.chars().zip(str.chars().skip(1)) {}
-    }
-    todo!()
-}
-
-// #[cfg(test)]
-// mod test_helpers {
-//     use super::reduce_string_to_range;
-//
-//     #[test]
-//     fn test_reduce_string_to_range() {
-//         let str: String = ('a'..='z').collect();
-//         reduce_string_to_range(&[str]);
-//     }
-// }
